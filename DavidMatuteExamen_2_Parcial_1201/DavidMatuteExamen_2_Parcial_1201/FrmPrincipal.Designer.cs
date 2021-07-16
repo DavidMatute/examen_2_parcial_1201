3fc5807319eec2d4792bdcd0114ba2c9fc8a84e7
DavidMatute <86425726+DavidMatute@users.noreply.github.com>
2021-07-15 22:13:48 -0600

    Primer commit
    Creación de la interfaz

---
+++ DavidMatuteExamen_2_Parcial_1201/DavidMatuteExamen_2_Parcial_1201/FrmPrincipal.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace DavidMatuteExamen_2_Parcial_1201
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Total_Pagar = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_Descuento = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txt_Subtotal = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_Precio_Medicamento = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_Cantidad = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_Presentacion = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Tipo_Medicamento = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Nomb_Medicamento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Direccion_Cl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Nombre_Cl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "\"FARMACIA EL CATRACHO\"";
+            // 
+            // txt_Total_Pagar
+            // 
+            this.txt_Total_Pagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Total_Pagar.Location = new System.Drawing.Point(646, 220);
+            this.txt_Total_Pagar.Name = "txt_Total_Pagar";
+            this.txt_Total_Pagar.Size = new System.Drawing.Size(76, 22);
+            this.txt_Total_Pagar.TabIndex = 45;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(498, 226);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(93, 16);
+            this.label11.TabIndex = 44;
+            this.label11.Text = "Total a Pagar:";
+            // 
+            // txt_Descuento
+            // 
+            this.txt_Descuento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Descuento.Location = new System.Drawing.Point(646, 170);
+            this.txt_Descuento.Name = "txt_Descuento";
+            this.txt_Descuento.Size = new System.Drawing.Size(76, 22);
+            this.txt_Descuento.TabIndex = 43;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(498, 176);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(76, 16);
+            this.label10.TabIndex = 42;
+            this.label10.Text = "Descuento:";
+            // 
+            // txt_Subtotal
+            // 
+            this.txt_Subtotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Subtotal.Location = new System.Drawing.Point(646, 113);
+            this.txt_Subtotal.Name = "txt_Subtotal";
+            this.txt_Subtotal.Size = new System.Drawing.Size(76, 22);
+            this.txt_Subtotal.TabIndex = 40;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(498, 119);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 16);
+            this.label9.TabIndex = 39;
+            this.label9.Text = "Subtotal:";
+            // 
+            // txt_Precio_Medicamento
+            // 
+            this.txt_Precio_Medicamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Precio_Medicamento.Location = new System.Drawing.Point(218, 328);
+            this.txt_Precio_Medicamento.Name = "txt_Precio_Medicamento";
+            this.txt_Precio_Medicamento.Size = new System.Drawing.Size(76, 22);
+            this.txt_Precio_Medicamento.TabIndex = 38;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(69, 334);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(135, 16);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "Precio Medicamento:";
+            // 
+            // txt_Cantidad
+            // 
+            this.txt_Cantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Cantidad.Location = new System.Drawing.Point(217, 294);
+            this.txt_Cantidad.Name = "txt_Cantidad";
+            this.txt_Cantidad.Size = new System.Drawing.Size(76, 22);
+            this.txt_Cantidad.TabIndex = 36;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(69, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 16);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Cantidad:";
+            // 
+            // txt_Presentacion
+            // 
+            this.txt_Presentacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Presentacion.Location = new System.Drawing.Point(218, 258);
+            this.txt_Presentacion.Name = "txt_Presentacion";
+            this.txt_Presentacion.Size = new System.Drawing.Size(121, 22);
+            this.txt_Presentacion.TabIndex = 34;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(69, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 16);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Presentación:";
+            // 
+            // txt_Tipo_Medicamento
+            // 
+            this.txt_Tipo_Medicamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Tipo_Medicamento.Location = new System.Drawing.Point(218, 220);
+            this.txt_Tipo_Medicamento.Name = "txt_Tipo_Medicamento";
+            this.txt_Tipo_Medicamento.Size = new System.Drawing.Size(121, 22);
+            this.txt_Tipo_Medicamento.TabIndex = 32;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(69, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 16);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Tipo de Medicamento:";
+            // 
+            // txt_Nomb_Medicamento
+            // 
+            this.txt_Nomb_Medicamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nomb_Medicamento.Location = new System.Drawing.Point(220, 186);
+            this.txt_Nomb_Medicamento.Name = "txt_Nomb_Medicamento";
+            this.txt_Nomb_Medicamento.Size = new System.Drawing.Size(217, 22);
+            this.txt_Nomb_Medicamento.TabIndex = 30;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(69, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 16);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Nombre Medicamento:";
+            // 
+            // txt_Direccion_Cl
+            // 
+            this.txt_Direccion_Cl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Direccion_Cl.Location = new System.Drawing.Point(218, 150);
+            this.txt_Direccion_Cl.Name = "txt_Direccion_Cl";
+            this.txt_Direccion_Cl.Size = new System.Drawing.Size(217, 22);
+            this.txt_Direccion_Cl.TabIndex = 28;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 16);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Dirección del Cliente:";
+            // 
+            // txt_Nombre_Cl
+            // 
+            this.txt_Nombre_Cl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nombre_Cl.Location = new System.Drawing.Point(218, 113);
+            this.txt_Nombre_Cl.Name = "txt_Nombre_Cl";
+            this.txt_Nombre_Cl.Size = new System.Drawing.Size(217, 22);
+            this.txt_Nombre_Cl.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(69, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Nombre del Cliente:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(41, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(427, 277);
+            this.listBox1.TabIndex = 24;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(474, 87);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(285, 173);
+            this.listBox2.TabIndex = 41;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 397);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(718, 298);
+            this.dataGridView1.TabIndex = 46;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 729);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_Total_Pagar);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txt_Descuento);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txt_Subtotal);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_Precio_Medicamento);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_Cantidad);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_Presentacion);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_Tipo_Medicamento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_Nomb_Medicamento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Direccion_Cl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_Nombre_Cl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.Text = "Boleta de Venta \"Farmacia El Catraco\"";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Total_Pagar;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_Descuento;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txt_Subtotal;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_Precio_Medicamento;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_Cantidad;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_Presentacion;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Tipo_Medicamento;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Nomb_Medicamento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Direccion_Cl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Nombre_Cl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ DavidMatuteExamen_2_Parcial_1201/DavidMatuteExamen_2_Parcial_1201/FrmUsuario.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace DavidMatuteExamen_2_Parcial_1201
+{
+    partial class FrmUsuario
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUsuario));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Usuario = new System.Windows.Forms.TextBox();
+            this.txt_Contraseña = new System.Windows.Forms.TextBox();
+            this.btn_Aceptar = new System.Windows.Forms.Button();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña:";
+            // 
+            // txt_Usuario
+            // 
+            this.txt_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Usuario.Location = new System.Drawing.Point(130, 44);
+            this.txt_Usuario.Name = "txt_Usuario";
+            this.txt_Usuario.Size = new System.Drawing.Size(239, 22);
+            this.txt_Usuario.TabIndex = 2;
+            // 
+            // txt_Contraseña
+            // 
+            this.txt_Contraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Contraseña.Location = new System.Drawing.Point(130, 93);
+            this.txt_Contraseña.Name = "txt_Contraseña";
+            this.txt_Contraseña.PasswordChar = '*';
+            this.txt_Contraseña.Size = new System.Drawing.Size(100, 22);
+            this.txt_Contraseña.TabIndex = 3;
+            // 
+            // btn_Aceptar
+            // 
+            this.btn_Aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Aceptar.Image = ((System.Drawing.Image)(resources.GetObject("btn_Aceptar.Image")));
+            this.btn_Aceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Aceptar.Location = new System.Drawing.Point(47, 173);
+            this.btn_Aceptar.Name = "btn_Aceptar";
+            this.btn_Aceptar.Size = new System.Drawing.Size(130, 43);
+            this.btn_Aceptar.TabIndex = 4;
+            this.btn_Aceptar.Text = "Aceptar";
+            this.btn_Aceptar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cancelar.Image = ((System.Drawing.Image)(resources.GetObject("btn_Cancelar.Image")));
+            this.btn_Cancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Cancelar.Location = new System.Drawing.Point(211, 173);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(130, 43);
+            this.btn_Cancelar.TabIndex = 5;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(387, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(163, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmUsuario
+            // 
+            this.AcceptButton = this.btn_Aceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Cancelar;
+            this.ClientSize = new System.Drawing.Size(597, 252);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.btn_Aceptar);
+            this.Controls.Add(this.txt_Contraseña);
+            this.Controls.Add(this.txt_Usuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FrmUsuario";
+            this.Text = "Ingrese al Sistema";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Usuario;
+        private System.Windows.Forms.TextBox txt_Contraseña;
+        private System.Windows.Forms.Button btn_Aceptar;
+        private System.Windows.Forms.Button btn_Cancelar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
